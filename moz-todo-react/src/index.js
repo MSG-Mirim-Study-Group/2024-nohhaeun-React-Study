707a0e14fd8223bc501f01765bac01d811cd3769
haeunNoh06 <okhagrace06@naver.com>
2023-12-29 16:39:58 +0900

    :sparkles: [feat] form, button, list 각각 컴포넌트화 시키기

--- moz-todo-react/src/index.js
+++ moz-todo-react/src/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false }
+];
+
+const BTN = [
+  { id: "btn-0", name: "All" },
+  { id: "btn-1", name: "Active" },
+  { id: "btn-2", name: "Completed" }
+];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App name="노하은"/>
+    <App name="노하은" tasks={DATA} buttons={BTN}/>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
