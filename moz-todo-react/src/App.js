87373c5ca5649ee26c145836e23d38f04e9b87c4
haeunNoh06 <okhagrace06@naver.com>
2024-01-04 22:07:37 +0900

    :sparkles: [feat] 할 일 추가 및 삭제 구현 완성

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -2,13 +2,32 @@ import React, { useState } from "react";
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
+import { nanoid } from "nanoid";
 
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
 
   function addTask(name) {
-    const newTask = { id: "id", name, completed: false };
+    console.log(`todo-${nanoid()}`);
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
     setTasks([...tasks, newTask]);
+  }
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      // 만약 이 할 일이 수정된 할 일로부터 같은 id를 가지고 있다면
+      if ( id === task.id ) {
+        return {...task, completed: !task.completed};
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id );
+    console.log(remainingTasks);
+    setTasks(remainingTasks);
   }
 
   const taskList = tasks.map((task) => ( 
@@ -16,7 +35,9 @@ function App(props) {
       id={task.id}
       name={task.name}
       completed={task.completed}
-      key={task.id} 
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
     /> 
   ));
   const btnNameList = props.buttons.map((btn) => (
@@ -25,14 +46,16 @@ function App(props) {
       key={btn.id}
     />
   ));
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
   return (
-    <div className="todoapp stack-large">
+    <div className="todo app stack-large">
       <h1>{props.name} 컴포넌트화 완성</h1>
       <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
         {btnNameList}
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
